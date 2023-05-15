e46829b11739611c045fee3fc022152d2d645d43
aniket-singh-01 <98407060+aniket-singh-01@users.noreply.github.com>
2023-05-15 18:37:53 +0530

    Completed

--- Development/BMI-Calculator/BMI Calculator/Views/CalculatorBrain.swift
+++ Development/BMI-Calculator/BMI Calculator/Views/CalculatorBrain.swift
@@ -1,14 +1,33 @@
+import UIKit
+
 struct CalculatorBrain{
     
-    var bmi: Float=0.0
+    var bmi: BMI?
+
     
     func getBMIValue() ->String {
-        let bmiTO1DecimalPlace=String(format: "%.1f", bmi)
+        let bmiTO1DecimalPlace=String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTO1DecimalPlace
+    }
+    func getAdvice() ->String{
+        return bmi?.advice ?? "No advice"
+    }
+    func getColor() ->UIColor{
+        return bmi?.color ?? .white
     }
     
     mutating func calculateBMI(height: Float,weight: Float){
-       bmi=weight/(height*height)
+        let bmiValue=weight/(height*height)
+        
+        if bmiValue<18.5{
+            bmi = BMI(value: bmiValue, advice: "Get more nutrients", color: UIColor.blue)
+        }
+        else if bmiValue<24.9{
+            bmi = BMI(value: bmiValue, advice: "Fit as Aniket ", color: UIColor.green)
+        }
+        else{
+            bmi = BMI(value: bmiValue, advice: "Go to gym", color: .red)
+        }
+        
     }
-
 }
---
+++ Development/demo/demo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  demo
+//
+//  Created by Aniket Singh on 15/05/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
