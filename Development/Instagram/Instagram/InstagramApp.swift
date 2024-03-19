5e50d82a058e2f94933e61e09fa1a6aace448207
Swift Nook <98407060+aniket-singh-01@users.noreply.github.com>
2024-03-19 20:04:50 +0530

    insta

---
+++ Development/Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Aniket Singh on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
